829d161f2b9a074159afb3e1a078180c6c0e248f
zibranhasan <zibranhasansourab@gmail.com>
2023-05-14 10:33:08 +0600

    final

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext} from 'react'
 import { CartContext, ProductContext } from './Root'
 import Product from './Product'
 import { addToDb } from '../utils/fakeDB'
